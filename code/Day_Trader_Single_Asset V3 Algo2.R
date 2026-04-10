e4cbf5b85f266f27ac17d43a9b2db481079b6a1f
Nikhil <nikhilchandra694@hotmail.com>
2026-04-11 09:22:10 +1000

    Code

--- code/Day_Trader_Single_Asset V3 Algo2.R
+++ code/Day_Trader_Single_Asset V3 Algo2.R
@@ -130,7 +130,7 @@ asset_list_oanda_single_asset <-
 
 #-------------Indicator Inputs
 
-end_time <- glue::glue("{floor_date(now(), 'week')} 04:59:00 AEST") %>% as_datetime(tz = "Australia/Canberra") + days(6)
+end_time <- glue::glue("{floor_date(now(), 'week')} 07:59:00 AEST") %>% as_datetime(tz = "Australia/Canberra") + days(6)
 current_time <- now()
 
 
